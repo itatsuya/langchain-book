5903cb161f02983b3f697af0f85c61dedd5c2422
tatsuya <tatsuya.ikuma@rolanddg.co.jp>
2023-11-20 04:20:23 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,153 @@
+import os
+from dotenv import load_dotenv
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+import re
+from langchain.chat_models import ChatOpenAI
+import time
+from typing import Any
+from langchain.schema import LLMResult
+from langchain.callbacks.base import BaseCallbackHandler
+from datetime import timedelta
+from langchain.memory import MomentoChatMessageHistory
+from langchain.schema import HumanMessage, LLMResult, SystemMessage
+import json
+import logging
+from slack_bolt.adapter.aws_lambda import SlackRequestHandler
+
+
+CHAT_UPDATE_INTERVAL_SEC = 1
+
+load_dotenv()
+
+
+# ログ
+SlackRequestHandler.clear_all_log_handlers()
+logging.basicConfig(
+        format="%(asctime)s [%(levelname)s] %(message)s", level=logging.INFO
+        )
+logger = logging.getLogger(__name__)
+
+
+
+# ボットトークンを使ってアプリを初期化します
+app = App(
+        signing_secret=os.environ["SLACK_SIGNING_SECRET"],
+        token=os.environ.get("SLACK_BOT_TOKEN"),
+        process_before_response=True,
+        )
+
+
+class SlackStreamingCallbackHandler(BaseCallbackHandler):
+    last_send_time = time.time()
+    message = ""
+
+    def __init__(self, channel, ts):
+        self.channel = channel
+        self.ts = ts
+        self.interval = CHAT_UPDATE_INTERVAL_SEC
+        self.update_count = 0
+
+    def on_llm_new_token(self, token:str, **kwargs) -> None:
+        self.message += token
+
+        now = time.time()
+        #if now - self.last_send_time > CHAT_UPDATE_INTERVAL_SEC:
+        if now - self.last_send_time > self.interval:
+            #self.last_send_time = now
+            app.client.chat_update(
+                    channel=self.channel, ts=self.ts, text=f"{self.message}\n\nTyyyyyyyyping..."
+                    )
+            
+            self.last_send_time = now
+            self.update_count += 1
+
+            if self.update_count / 10 > self.interval:
+                self.interval = self.interval * 2
+
+    def on_llm_end(self, response:LLMResult, **kwargs:Any) -> Any:
+        message_context = "Open AI APIで生成される情報は不正確または不適切な場合がありますが、当社の見解を述べるものではありません。"
+        message_blocks = [
+                {"type":"section", "text": {"type": "mrkdwn", "text":self.message}},
+                {"type":"divider"},
+                {"type":"context",
+                 "elements":[{"type":"mrkdwn", "text":message_context}],
+                 },
+                ]
+        app.client.chat_update(
+                channel=self.channel, 
+                ts=self.ts, 
+                text=self.message,
+                block=message_blocks,
+                )
+
+
+#@app.event("app_mention")
+def handle_mention(event, say):
+    channel = event["channel"]
+    #user = event["user"]
+    thread_ts = event["ts"]
+    message = re.sub("<@.*>", "", event["text"])
+
+    #投稿のキー(=Momentoキー)：初回=event["ts"],2回目以降=event["thread_ts"]
+    id_ts = event["ts"]
+    if "thread_ts" in event:
+        id_ts = event["thread_ts"]
+
+    result = say("\n\nTyping...", thread_ts=thread_ts)
+    ts = result["ts"]
+
+    history = MomentoChatMessageHistory.from_client_params(
+            id_ts,
+            os.environ["MOMENTO_CACHE"],
+            timedelta(hours=int(os.environ["MOMENTO_TTL"])),
+            )
+
+    messages = [SystemMessage(content="You are a good assistant.")]
+    messages.extend(history.messages)
+    messages.append(HumanMessage(content=message))
+
+    history.add_user_message(message)
+
+
+    callback = SlackStreamingCallbackHandler(channel=channel, ts=ts)
+
+    llm = ChatOpenAI(
+            model_name=os.environ["OPENAI_API_MODEL"],
+            temperature=os.environ["OPENAI_API_TEMPERATURE"],
+            streaming=True,
+            callbacks=[callback],
+            )
+    
+    ai_message = llm(messages)
+    history.add_message(ai_message)
+
+    #llm.predict(message)
+    #response = llm.predict(message)
+    #say(text=response, thread_ts=thread_ts)
+
+def just_ack(ack):
+    ack()
+
+app.event("app_mention")(ack=just_ack, lazy=[handle_mention])
+
+
+
+if __name__ == "__main__":
+    SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
+
+
+
+def handler(event, context):
+    logger.info("handler called")
+    header = event["headers"]
+    logger.info(json.dumps(header))
+
+    if "x-slack-retry-num" in header:
+        logger.info("SKIP > x-slack-retry-num: %s", header["x-slack-retry-num"])
+        return 200
+
+    #AWS Lambda環境のリクエスト情報をappが処理できるように変換してくれるアダプター
+    slack_handler = SlackRequestHandler(app=app)
+    #未応答はそのままAWS Lambdaの戻り値として返せます
+    return slack_handler.handle(event, context)
